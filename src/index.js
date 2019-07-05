a40d478f353ae96de382b33008370a1447b05433
Jack Linton <jacklinton@mac.com>
2019-07-05 10:26:32 -0400

    WIP commit

--- src/index.js
+++ src/index.js
@@ -24,3 +24,44 @@ function mungeData() {
 
     renderChart(regionsPctTurnout);
 }
+
+function renderChart(data) {
+    chart.attr('width', window.innerWidth)
+        .attr('height', window.innerHeight)
+}
+
+renderChart();
+
+const x = d3.scaleBand()
+    .domain(data.map(d => d.region))
+    .rangeRound([50, window.innerWidth - 50])
+    .padding(0.1);
+
+const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.meanPctTurnout)])
+    .range([window.innerHeight - 50, 0]);
+
+const xAxis = d3.axisBottom().scale(x);
+const yAxis = d3.axisLeft().scale(y);
+
+chart.append('g')
+    .attr('class', 'axis')
+    .attr('transform',
+        `&grave;translate(0, ${window.innerHeight -50})&grave;`)
+    .call(xAxis);
+
+chart.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(50, 0')
+    .call(yAxis);
+
+chart.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.region))
+    .attr('y', d => y(d.meanPctTurnout))
+    .attr('width', x.bandwidth())
+    .attr('height', d => (window.innerHeight - 50) - y(d.meanPctTurnout));
+
