44fa05881dc10bcb625d527f63ba86fd63ccfe71
Jack Linton <john.linton@jefferson.edu>
2019-07-05 14:37:33 -0400

    Fix functioning chart but styles need mending

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import '../styles/index.css';
 
 const chart = d3.select('body')
     .append('svg')
@@ -19,7 +20,7 @@ function mungeData() {
     const regionsPctTurnout = Object.entries(regions)
         .map(([region, areas]) => ({
             region,
-            meanPctTurnout: d3.mean(areas, d => p.Pct_Turnout),
+            meanPctTurnout: d3.mean(areas, d => d.Pct_Turnout),
         }));
 
     renderChart(regionsPctTurnout);
@@ -28,40 +29,38 @@ function mungeData() {
 function renderChart(data) {
     chart.attr('width', window.innerWidth)
         .attr('height', window.innerHeight)
+
+    const x = d3.scaleBand()
+        .domain(data.map(d => d.region))
+        .rangeRound([50, window.innerWidth - 50])
+        .padding(0.1);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.meanPctTurnout)])
+        .range([window.innerHeight - 50, 0]);
+
+    const xAxis = d3.axisBottom().scale(x);
+    const yAxis = d3.axisLeft().scale(y);
+
+    chart.append('g')
+        .attr('class', 'axis')
+        .attr('transform',
+            `&grave;translate(0, ${window.innerHeight -50})&grave;`)
+        .call(xAxis);
+
+    chart.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(50, 0')
+        .call(yAxis);
+
+    chart.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => x(d.region))
+        .attr('y', d => y(d.meanPctTurnout))
+        .attr('width', x.bandwidth())
+        .attr('height', d => (window.innerHeight - 50) - y(d.meanPctTurnout));
 }
-
-renderChart();
-
-const x = d3.scaleBand()
-    .domain(data.map(d => d.region))
-    .rangeRound([50, window.innerWidth - 50])
-    .padding(0.1);
-
-const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.meanPctTurnout)])
-    .range([window.innerHeight - 50, 0]);
-
-const xAxis = d3.axisBottom().scale(x);
-const yAxis = d3.axisLeft().scale(y);
-
-chart.append('g')
-    .attr('class', 'axis')
-    .attr('transform',
-        `&grave;translate(0, ${window.innerHeight -50})&grave;`)
-    .call(xAxis);
-
-chart.append('g')
-    .attr('class', 'axis')
-    .attr('transform', 'translate(50, 0')
-    .call(yAxis);
-
-chart.selectAll('rect')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar')
-    .attr('x', d => x(d.region))
-    .attr('y', d => y(d.meanPctTurnout))
-    .attr('width', x.bandwidth())
-    .attr('height', d => (window.innerHeight - 50) - y(d.meanPctTurnout));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,42 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
+            // {
+            //   test: /\.js$/,
+            //   loader: 'eslint-loader',
+            //   exclude: /node_modules/,
+            // },
+            // {
+            //     test: /\.ts?$/, // Another convention is to use the .es6 filetype, but you then
+            //     // have to supply that explicitly in import statements, which isn't cool.
+            //     exclude: [/(node_modules|bower_components)/],
+            //     loader: 'ts-loader',
+            //     // loaders: ['babel-loader', 'ts-loader'],
+            // },
+            {
+                test: /\.js?$/, // Another convention is to use the .es6 filetype, but you then
+                // have to supply that explicitly in import statements, which isn't cool.
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+            },
+            // This nifty bit of magic right here allows us to load entire JSON files
+            // synchronously using `require`, just like in NodeJS.
+            {
+                test: /\.json$/,
+                loader: 'json-loader',
+            },
+            // This allows you to `require` CSS files.
+            // We be in JavaScript land here, baby! No <style> tags for us!
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+            },
+        ],
     },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    }
+    plugins: [
+        new HtmlWebpackPlugin({template: './index.html'})
+    ]
 };
